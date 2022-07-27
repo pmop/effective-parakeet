d5fb2f3227b2bbde93ea6ba4ad0ac4425ca3e872
Pedro Pinheiro <pedro@tidy.com>
2022-07-27 07:58:03 -0300

    (TDD - Baby steps) implement log parser behavior with invalid file path

--- parser.rb
+++ parser.rb
@@ -4,7 +4,14 @@ Dir['./lib/**/*.rb'].sort.each { |file| require file }
 no_args_error_msg = 'No argument received. Please, '+
                     'run logparse with a valid file path to a valid log file.'
 
+file_not_found_error_msg = 'File not found. Please, make sure the file path is valid.'
+
 if ARGV.empty? || ARGV[0].empty?
   warn(no_args_error_msg)
+  exit(1)
+end
+
+if !File.exist?(ARGV[0])
+  warn(file_not_found_error_msg)
   exit(1)
 end
