a789783a10e59782d5ead1462736ab736f3ae7b1
Pedro Pinheiro <pedro@tidy.com>
2022-07-27 06:45:38 -0300

    (TDD - Baby steps) test Parser behavior with empty data

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -1,11 +1,21 @@
 RSpec.describe Parser do
-  subject { Parser.new(data: data) }
+  subject { described_class.new(data: data) }
   describe '.new' do
     context "when parameter 'data' value is not valid" do
       it 'raises the correct error' do
         expect { described_class.new(data: nil) }.to raise_error(
           'Unexpected parameter "data" value type. "data" is expected to be a "String"'
         )
+      end
+    end
+  end
+
+  describe '#call' do
+    context 'when data is empty string' do
+      let(:data) { '' }
+
+      it 'returns empty array' do
+        expect(subject.call).to eq([])
       end
     end
   end
