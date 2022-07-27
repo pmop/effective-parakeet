c23a8d7e5cdc5c45c52b8a7fd4c96d6cc7f788df
Pedro Pinheiro <pedro@tidy.com>
2022-07-27 07:38:06 -0300

    (TDD - Baby steps) test logparser script behavior with no argument

--- spec/logparser_spec.rb
+++ spec/logparser_spec.rb
@@ -5,12 +5,10 @@ RSpec.describe 'Log parser' do
     let(:script_arg) { '' }
 
     it 'raises the correct error' do
-      error_msg = 'No argument received. Please, '+
+      error_msg = 'No argument received. Please, ' +
                   'run logparse with a valid file path to a valid log file.'
 
-      expect { invoke_script }.to raise_error(
-        Errors::NoArgumentsError, error_msg
-      )
+      expect { invoke_script }.to output(a_string_including(error_msg)).to_stderr_from_any_process
     end
   end
 end
