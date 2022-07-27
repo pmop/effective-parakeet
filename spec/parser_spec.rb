e23a58a494caf57c30dbe67abbb15085c018ad76
Pedro Pinheiro <pedro@tidy.com>
2022-07-27 08:17:05 -0300

    (TDD - Baby steps) implement log parser behavior with valid file path

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'Log parser' do
       info_msg = 'No argument received. Please, ' +
                   'run logparse with a valid file path to a valid log file.'
 
-      expect { invoke_script }.to output(a_string_including(info_msg)).to_stderr_from_any_process
+      expect { invoke_script }.to output(a_string_including(info_msg))
+        .to_stderr_from_any_process
     end
   end
 
@@ -18,7 +19,24 @@ RSpec.describe 'Log parser' do
     it 'raises the correct info' do
       info_msg = 'File not found. Please, make sure the file path is valid.'
 
-      expect { invoke_script }.to output(a_string_including(info_msg)).to_stderr_from_any_process
+      expect { invoke_script }.to output(a_string_including(info_msg))
+        .to_stderr_from_any_process
+    end
+  end
+
+  context 'when running script with valid file' do
+    let(:script_arg) { ' ./spec/fixtures/simple.log' }
+    let(:expected_output) do
+        <<-HEREDOC
+        /help_page/1 2 unique views
+        /contact 1 unique views
+        /home 1 unique views
+        HEREDOC
+    end
+
+    it 'returns the correct output' do
+      expect { invoke_script }.to output(a_string_including(expected_output))
+        .to_stdout_from_any_process
     end
   end
 end
